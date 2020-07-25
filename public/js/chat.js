879c0c6f8dfabe61f6ba4920dd031326ecdc2ccc
sumit <sumit.pradhan65@gmail.com>
2020-07-25 19:33:46 +0530

    package.json

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,20 +18,22 @@ socket.on('location', (loc)=>{
     console.log(loc)
 })
 
-document.querySelector('#increment').addEventListener('click', ()=>{
-    console.log('Click')
-    socket.emit('increment')
-})
 document.querySelector('#send').addEventListener('click', (e)=>{
     e.preventDefault()
-
     const msg = document.querySelector('#message').value
     if (msg !== ''){
-        socket.emit('message', msg)
+        socket.emit('message', msg, (error)=>{
+            if (error){
+                return console.log(error)
+            }
+            console.log('message was delivered') // Event Acknowledgement. server runs this fn when io.emit() runs
+        })
     }
 })
 document.querySelector('#location').addEventListener('click', ()=>{
     navigator.geolocation.getCurrentPosition((location)=>{
-        socket.emit('location', location)
+        socket.emit('location', location, ()=>{
+            console.log('Location Delivered')
+        })
     })
 })
